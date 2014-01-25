483ab3f2f07573e4fe0658eb56abaa5862d4c679
dal <dal.adamson@gmail.com>
2014-01-25 10:30:30 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daladamson::Application.config.secret_key_base = 'ec116f7625ca328a5a0785b974654aaea50ce717f5079516161ab790d600f7bea8f233b7c42e7f11703d5e7e4ca6aac4b4e5e2dd570101f40ede14b871e46aa7'
